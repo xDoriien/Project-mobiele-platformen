c3ba6d7d8ffc888554a3e6125147e316694fa8c6
Dorien Truyens <Doriientruyens@hotmail.com>
2013-08-13 16:21:23 +0200

    pagina basisklas
    pagina basisklas als men ingelogd wordt klaar, mogelijk om een klas te
    selecteren
    de vakken zelf moeten nog kunnen worden aangevinkt

---
+++ ingelogd.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+<link href="css/mobile.css" rel="stylesheet" type="text/css" />
+
+<title>3IMDA Truyens Dorien</title>
+</head>
+
+<body>
+	<div id="header">
+    	<img src="images/tshirt.jpg" alt="header"/>
+    </div>
+    <div id="selectionbox">
+    <p style="font:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; color:#4ba1af; margin-left: 120px;" >Kies uw basisklas</p>
+    <select style="margin-left:170px">
+  		<option value="3IMDA">3IMDA</option>
+  		<option value="3IMDB">3IMDB</option>
+	</select>
+    </div>
+    <div id="klassen" style="margin-top: 20px;">
+    <p style="color:#4ba1af; margin-left:20px;">Rich Media Applications</p>
+    <p style="color:#4ba1af; margin-left:20px;">Communicatieve Vaardigheden</p>
+    <p style="color:#4ba1af; margin-left:20px;">Engels - Frans</p>
+    <p style="color:#4ba1af; margin-left:20px;">Visualiseren 3</p>
+    <p style="color:#4ba1af; margin-left:20px;">Actualia</p>
+    <p style="color:#4ba1af; margin-left:20px;">Mobiele platformen</p>
+    <p style="color:#4ba1af; margin-left:20px;">Marketingcommunicatie</p>
+    </div>
+    <div id="button" style="margin-left:140px; margin-top:10px;">
+    	<a href="extraklas.php"><img src="images/volgende_btn.jpg" alt="header"/></a>
+    </div>
+    
+</body>
+</html>
