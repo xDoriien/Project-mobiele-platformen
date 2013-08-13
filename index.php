b9bfdfbcb27a511bf936ee071d06ba24377456f6
Dorien Truyens <Doriientruyens@hotmail.com>
2013-08-13 14:06:42 +0200

    index + login
    de gebruikers kunnen nu de eerste pagina zien en inloggen

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+<link href="css/mobile.css" rel="stylesheet" type="text/css" media="only screen and (max-width: 320px), only screen and (max-device-width: 320px)" />
+
+<title>3IMDA Truyens Dorien</title>
+</head>
+
+<body>
+	<div id="header"><img src="images/header.png" alt="header"/></div>
+    
+</body>
+</html>
