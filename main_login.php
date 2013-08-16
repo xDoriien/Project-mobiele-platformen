aa578929e90cb34d3c6b30b7864e9b7befd99fb2
Dorien Truyens <Doriientruyens@hotmail.com>
2013-08-16 17:34:35 +0200

    Lessen worden ingelezen uit databank in checkboxlist

--- main_login.php
+++ main_login.php
@@ -15,32 +15,38 @@
     <div id="selectionbox">
     <p style="font-family:'Trebuchet MS', Arial, Helvetica, sans-serif; color:#4ba1af; margin-left: 55px;" >Kies uw basisklas:
     <?php
-	mysql_connect('localhost', 'root', 'root');
-	mysql_select_db('Mobiele_platformen');
-
-	$sql = "SELECT name FROM classes";
-	$result = mysql_query($sql);
-
-	echo "<select name='name'>";
-	while ($row = mysql_fetch_array($result)) {
-    echo "<option value='" . $row['name'] . "'>" . $row['name'] . "</option>";
-	}
-	echo "</select>";
-
+		mysql_connect('localhost', 'root', 'root');
+		mysql_select_db('Mobiele_platformen');
+	
+		$sql = "SELECT name FROM classes";
+		$result = mysql_query($sql);
+	
+		echo "<select name='name'>";
+		while ($row = mysql_fetch_array($result)) {
+		echo "<option value='" . $row['name'] . "'>" . $row['name'] . "</option>";
+		}
+		echo "</select>";
 	?>
     </p>
     </div>
     <div id="klassen" style="margin-top:20px;margin-left:20px;color:#4ba1af;font-family:'Trebuchet MS', Arial, Helvetica, sans-serif">
+   
+    
+
     <form method="post" action="extraclasses.php" >
-        <input name="vak1" type="checkbox" value="vakken"  checked="true"/> Rich Media applications</br>
-        <input name="vak2" type="checkbox" value="vakken"  checked="true"/> Communicatieve vaardigheden</br>
-        <input name="vak3" type="checkbox" value="vakken"  checked="true"/> Engels - Frans</br>
-        <input name="vak4" type="checkbox" value="vakken"  checked="true"/> Visualiseren</br>
-        <input name="vak5" type="checkbox" value="vakken"  checked="true"/> Actualia</br>
-        <input name="vak6" type="checkbox" value="vakken"  checked="true"/> Mobiele platformen</br>
-        <input name="vak7" type="checkbox" value="vakken"  checked="true"/> Marketingcommunicatie</br>
-        <input name="vak8" type="checkbox" value="vakken"  checked="true"/> Recht</br>
-        <input name="vak9" type="checkbox" value="vakken"  checked="true"/> Indesign</br>
+    <?php
+		mysql_connect('localhost', 'root', 'root');
+		mysql_select_db('Mobiele_platformen');
+	
+		$sql = "SELECT name FROM lessons";
+		$result = mysql_query($sql);
+	
+	
+		while ($row = mysql_fetch_array($result)) {
+		echo "<input type='checkbox'" . $row['name'] . "'>" . $row['name'] . "</br> </option>";
+		}
+		echo "</select>";
+	?>
         <input type="image" name="submit" value="submit" src="images/volgende_btn.jpg"  style="margin-left:140px; margin-top:10px;" />
 	</form>
  	</div>
