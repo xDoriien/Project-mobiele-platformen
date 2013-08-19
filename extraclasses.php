6204aef2339559ac8e23c93289f2e61788d43b33
Dorien Truyens <Doriientruyens@hotmail.com>
2013-08-19 15:02:26 +0200

    Extra klassen
    Extra klassen kunnen worden aangeduid.

--- extraclasses.php
+++ extraclasses.php
@@ -13,30 +13,43 @@
     	<img src="images/tshirt.jpg" alt="header"/>
     </div>
     <div id="selectionbox">
-    <p style="font-family:'Trebuchet MS', Arial, Helvetica, sans-serif; color:#4ba1af; margin-left: 80px;" >Vul aan met extra lessen</p>
-    <select style="margin-left:190px">
-  		<option value="2IMDA">2IMDA</option>
-  		<option value="2IMDB">2IMDB</option>
-        <option value="2IMDC">2IMDC</option>
-        <option value="1IMDA">1IMDA</option>
-  		<option value="1IMDB">1IMDB</option>
-        <option value="1IMDC">1IMDC</option>
-	</select>
+    <p style="font-family:'Trebuchet MS', Arial, Helvetica, sans-serif; color:#4ba1af; margin-left: 55px;" >Extra klassen:
+    <?php
+		mysql_connect('localhost', 'root', 'root');
+		mysql_select_db('Mobiele_platformen');
+	
+		$sql = "SELECT name FROM classes";
+		$result = mysql_query($sql);
+	
+		echo "<select name='name'>";
+		while ($row = mysql_fetch_array($result)) {
+		echo "<option value='" . $row['name'] . "'>" . $row['name'] . "</option>";
+		}
+		echo "</select>";
+	?>
+    </p>
     </div>
     <div id="klassen" style="margin-top:20px;margin-left:20px;color:#4ba1af;font-family:'Trebuchet MS', Arial, Helvetica, sans-serif">
-     <form method="post" action="extraclasses.php" >
-        <input name="vak1" type="checkbox" value="vakken" checked="true"/> IRMA</br>
-        <input name="vak2" type="checkbox" value="vakken" checked="true"/> Frans 2</br>
-        <input name="vak3" type="checkbox" value="vakken" checked="true"/> Engels 2</br>
-        <input name="vak4" type="checkbox" value="vakken" checked="true"/> Visualiseren 2</br>
-        <input name="vak5" type="checkbox" value="vakken" checked="true"/> CMS</br>
-        <input name="vak6" type="checkbox" value="vakken" checked="true"/> E-business</br>
-        <input name="vak7" type="checkbox" value="vakken" checked="true"/> Photoshop 2</br>
-        <input name="vak8" type="checkbox" value="vakken" checked="true"/> Photoshop 1</br>
-        <input name="vak9" type="checkbox" value="vakken" checked="true"/> Frans 1</br>
-        <input name="vak9" type="checkbox" value="vakken" checked="true"/> Engels 1</br>
+   
+    
+
+    <form method="post" action="extraclasses.php" >
+    <?php
+		mysql_connect('localhost', 'root', 'root');
+		mysql_select_db('Mobiele_platformen');
+	
+		$sql = "SELECT name FROM lessons";
+		$result = mysql_query($sql);
+	
+	
+		while ($row = mysql_fetch_array($result)) {
+		echo "<input type='checkbox'" . $row['name'] . "'>" . $row['name'] . "</br> </option>";
+		}
+		echo "</select>";
+	?>
         <input type="image" name="submit" value="submit" src="images/volgende_btn.jpg"  style="margin-left:140px; margin-top:10px;" />
 	</form>
-    </div>
+ 	</div>
+
 </body>
 </html>
