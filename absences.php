1abd8f0a00a12442cb5e260b800e2eeac7f4e4c3
Dorien Truyens <Doriientruyens@hotmail.com>
2013-08-19 16:00:42 +0200

    Afwezigheden
    Afwezigheden worden uit de database gelezen en zijn nu zichtbaar voor
    de gebruiker

--- absences.php
+++ absences.php
@@ -12,5 +12,26 @@
  <div id="instellingen" style="margin-left:230px;margin-top:-55px;">
  <a href="main_login.php"><img src="images/tandwiel.jpg" alt="instellingen"/></a>
  </div>
+ <div id="announcements"  style="margin-left:15px;color:#4ba1af;font-family:'Trebuchet MS', Arial, Helvetica, sans-serif">
+ <br />
+ <br />
+ 	<?php
+		mysql_connect('localhost', 'root', 'root');
+		mysql_select_db('Mobiele_platformen');
+	
+		$sql = "SELECT * FROM absences";
+		$result = mysql_query($sql);
+	
+		while ($row = mysql_fetch_array($result)) {
+			echo $row['teacher'];
+			echo "<br/><i style=font-size:13px;>";
+			echo $row['announcement'] ;
+			echo "</i>";
+			echo '<hr style="border: 0,7px solid #db5644; width:300px; margin-left:-0px;"/>';
+		}
+		echo "</select>";
+		
+	 ?>
+ </div>
 </body>
 </html>
